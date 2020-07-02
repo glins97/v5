40ff030f175e093f2c2b1fa4e2062e4e586358ed
Gabriel Lins <nombregag@gmail.com>
2020-07-02 17:03:11 -0300

    TPS now deletes/updates other question items

--- tps/admin.py
+++ tps/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import include, path, re_path
 from django.utils.html import format_html
 from django.http import FileResponse
+from django import forms
+
 from tps.auxiliary import generate_score_z, generate_tbl, generate_distrator
-from .models import TPS, Answer
+from .models import TPS, Answer, Question
 
 import subprocess
 
@@ -12,8 +14,85 @@ class AnswerAdmin(admin.ModelAdmin):
     list_filter = ('tps', 'grade', )
     # search_fields = (,)
     list_per_page = 100
- 
+
+from django.contrib.admin.utils import flatten_fieldsets
+
+def create_or_update(class_, **kwargs):
+    search = class_.objects.filter(**kwargs) 
+    if search.count():
+        return search.get()
+    obj = class_(**kwargs)
+    obj.save()
+    return obj
+
+class TPSAdminForm(forms.ModelForm):
+    class Meta:
+        model = TPS
+        exclude = ('', )
+
+    def __init__(self, *args, **kwargs):
+        super(TPSAdminForm, self).__init__(*args, **kwargs)
+        questions = {i: 'NA' for i in range(1, 1000)}
+        saved_questions = Question.objects.filter(tps__id=self.instance.id)
+        for question in saved_questions:
+            questions[question.number] = question.correct_answer
+
+        for question_number in sorted(questions.keys()):
+            self.fields[f'q{question_number}'] = forms.ChoiceField(choices=[('NA', 'NA'), ('A', 'A'), ('B', 'B'), ('C', 'C'), ('D', 'D'), ('E', 'E')], initial=questions[question_number])
+
+    def save(self, commit=True):
+        instance = super(TPSAdminForm, self).save(commit=False)
+        print(repr(instance))
+        for index, field in enumerate([attr for attr in self.cleaned_data if attr[0] == 'q']):
+            if self.cleaned_data[field] != 'NA':
+                # gets matching model or creates object if no matches exist 
+                q = create_or_update(Question, tps=TPS.objects.get(id=instance.id), number=(index + 1))
+                
+                # updates only necessary questions, avoiding creation of duplicates
+                if q.correct_answer != self.cleaned_data[field]: 
+                    print(f'UPDATING FIELD {field}') 
+                    q.correct_answer = self.cleaned_data[field]
+                    q.save()
+
+                # delete questions no longer used
+                print(q.number, self.cleaned_data['max_questions'], q.number > self.cleaned_data['max_questions'])
+                if q.number > self.cleaned_data['max_questions']:
+                    q.delete() 
+        if commit:
+            instance.save()
+        return instance
+
 class TPSAdmin(admin.ModelAdmin):
+    form = TPSAdminForm
+    
+    fieldsets = (
+        ('Conteúdo', {
+            'fields': ('subject', 'week', 'campus',),
+        }),
+        ('Informações', {
+            'fields': ('max_answers', 'max_questions', 'tbl', 'score_z', 'distractor'),
+        }),
+        ('Data', {
+            'fields': ('start_date', 'end_date',),
+        }),
+    )
+    def get_form(self, request, obj=None, **kwargs):
+        kwargs['fields'] =  flatten_fieldsets(self.fieldsets)
+        return super(TPSAdmin, self).get_form(request, obj, **kwargs)
+
+    def get_fieldsets(self, request, obj=None):
+        fieldsets = super(TPSAdmin, self).get_fieldsets(request, obj)
+        newfieldsets = list(fieldsets)
+        print(fieldsets)
+        fields = [f'q{item}' for item in range(1, 1000)]
+        newfieldsets.append(['Questões', { 'fields': fields }])
+        return newfieldsets
+
+    class Media:
+        js = (
+            '//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
+            '/static/tps/js/fields.js',
+        )
     list_display = ('id', 'campus', 'subject', 'week', 'respostas', 'url', 'relatórios',)
     list_filter = ('campus', 'subject', 'week',)
     search_fields = ('campus', 'subject', 'week', )
@@ -67,5 +146,27 @@ class TPSAdmin(admin.ModelAdmin):
         return self._gen_pdf(id, 'distrator')
 
 
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('tps_id', 'campus', 'subject', 'week', 'questão', 'correct_answer', )
+    list_filter = ('tps__subject', 'tps__campus', )
+    search_fields = ('tps', 'q', 'tps__subject', 'tps__campus', )
+    list_per_page = 100
+
+    def tps_id(self, obj):
+        return obj.tps.id
+
+    def campus(self, obj):
+        return obj.tps.campus
+    
+    def subject(self, obj):
+        return obj.tps.subject
+
+    def week(self, obj):
+        return obj.tps.week
+
+    def questão(self, obj):
+        return 'Questão {}'.format(obj.number)
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(TPS, TPSAdmin)
 admin.site.register(Answer, AnswerAdmin)
