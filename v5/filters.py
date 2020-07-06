de9d85d87c9e97bd8b7db71279cf6f88ddc0f041
Gabriel Lins <gabriel.lins97@gmail.com>
2020-07-06 13:41:09 +0000

    FEAT: logs now send full error log to email

--- v5/filters.py
+++ v5/filters.py
@@ -1,10 +1,16 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from email.encoders import encode_base64
 import logging
+import traceback
+from time import sleep
+from threading import _start_new_thread
 
 logger = logging.getLogger('django')
-def send_mail(message):
+def send_mail():
+    sleep(1)
     try:
         msg = MIMEMultipart()
         password = 'campusppa'
@@ -12,16 +18,23 @@ def send_mail(message):
         msg['From'] = 'adm.ppa.digital@gmail.com'
         msg['Subject'] = 'Error@PPA_Digital'
 
-        msg.attach(MIMEText(message, 'html'))
+        openedfile = None
+        attachment = 'logs/error.log'
+        with open(attachment, 'rb') as opened:
+            openedfile = opened.read()
+        attachedfile = MIMEApplication(openedfile, _subtype = "txt", _encoder=encode_base64)
+        attachedfile.add_header('content-disposition', 'attachment', filename=attachment.split('/')[-1])
+        msg.attach(attachedfile)
+
         with smtplib.SMTP('smtp.gmail.com: 587') as server:
             server.starttls()
             server.login(msg['From'], password)
             server.sendmail(msg['From'], msg['To'], msg.as_string())
         return True
     except Exception as e:
-        logger.error(f'send_mail@filters::Failed to send mail | {e}')
+        logger.warning(f'send_mail@filters::Failed to send mail | {e}')
         return False
 
 def mail_errors(record):
-    send_mail(str(record))
+    _start_new_thread(send_mail, ())
     return True
