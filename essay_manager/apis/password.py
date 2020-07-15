5e32aa38aeee334924719cf06d94b4028db757e4
Gabriel Lins <nombregag@gmail.com>
2020-07-15 15:16:34 -0300

    CHNG: logger of password change api refactored

--- essay_manager/apis/password.py
+++ essay_manager/apis/password.py
@@ -7,10 +7,10 @@ logger = logging.getLogger()
 
 @login_required
 def change_password_endpoint(request):
+    username = request.user.username
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
     try:
-        username = request.user.username
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
         if user is not None:
             if request.POST['password-new'] == request.POST['password-new-confirmation']:
                 user.set_password(request.POST['password-new'])
@@ -20,5 +20,5 @@ def change_password_endpoint(request):
             return redirect('/profile/?updated=False')
         return redirect('/profile/?updated=False')
     except Exception as e:
-        logger.error('exception@change_password_endpoint ->', e)
+        logger.error(f'Error changing user {user} password. Error {e}', exc_info=e)
         return redirect('/profile/?updated=False')
