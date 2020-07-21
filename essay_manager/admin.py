c22502e8ce77e8252984fd44e0acb7b3fc596111
Gabriel Lins <gabriel.lins97@gmail.com>
2020-07-21 15:35:38 +0000

    FEAT: admin interface reworked for corrections and essays

--- essay_manager/admin.py
+++ essay_manager/admin.py
@@ -1,8 +1,48 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Theme, Essay, Profile, Correction, ErrorClassification, GenericErrorClassification
 
 class EssayAdmin(admin.ModelAdmin):
     autocomplete_fields = ('user', )
+    list_display = ('id', 'aluno', 'tema', 'nota', )
+    list_filter = ('theme', )
+    search_fields = ('user__first_name', 'user__last_name', 'theme__description')
+    list_per_page = 100
+
+    def aluno(self, o):
+        return format_html(
+            '<a href=/admin/auth/user/{}/change/>{}</a>'.format(
+                o.user.id,
+                o.user.first_name + ' ' + o.user.last_name))
+
+    def tema(self, o):
+        return o.theme.description
+
+    def nota(self, o):
+        return o.grade
+
+class CorrectionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'id_redacao', 'corretor', 'aluno', 'tema', 'status', 'nota', )
+    list_per_page = 100
+    search_fields = ('essay__user__first_name', 'essay__user__last_name', 'essay__theme__description')
+
+    def id_redacao(self, o):
+        return o.essay.id
+
+    def corretor(self, o):
+        return o.user.first_name + ' ' + o.user.last_name 
+
+    def aluno(self, o):
+        return format_html(
+            '<a href=/admin/auth/user/{}/change/>{}</a>'.format(
+                o.essay.user.id,
+                o.essay.user.first_name + ' ' + o.essay.user.last_name))
+
+    def tema(self, o):
+        return o.essay.theme.description
+
+    def nota(self, o):
+        return o.essay.grade
 
 class GenericErrorClassificationAdmin(admin.ModelAdmin):
     list_display = ('codigo', 'name', 'pai', 'nota', )
@@ -37,6 +77,6 @@ class ErrorClassificationAdmin(admin.ModelAdmin):
 admin.site.register(Theme)
 admin.site.register(Essay, EssayAdmin)
 admin.site.register(Profile)
-admin.site.register(Correction)
+admin.site.register(Correction, CorrectionAdmin)
 admin.site.register(ErrorClassification, ErrorClassificationAdmin)
 admin.site.register(GenericErrorClassification, GenericErrorClassificationAdmin)
