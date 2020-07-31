717ca976db58aa8eb4bad2c9e5ca06d512cce110
Gabriel Lins <gabriel.lins97@gmail.com>
2020-07-31 12:56:26 +0000

    CHGN: tpses can now always be visible and only answers are restricted

--- tps/views.py
+++ tps/views.py
@@ -9,21 +9,42 @@ def tps_view(request, id, campus, subject, week):
         answers = TPSAnswer.objects.all().filter(tps=tps)
         if len(answers) >= tps.max_answers:
             return render(request, 'feed.html', {'title': 'Opa!', 'description': 'Número limite de respostas já atingido.'})
-        if tps.start_date > now():
-            return render(request, 'feed.html', {'title': 'Opa!', 'description': 'Respostas serão liberadas apenas no dia {}.'.format(tps.start_date.strftime("%d/%m/%Y às %H:%M:%S"))})
-        if tps.end_date < now():
-            return render(request, 'feed.html', {'title': 'Opa!', 'description': 'Tempo limite de resposta excedido.'})
+        
+        if tps.questions:
+            data = {
+                'id': id,
+                'subject_desc': {
+                    'QUI': 'Química',
+                    'BIO': 'Biologia',
+                    'MAT': 'Matemática',
+                    'FIS': 'Física',
+                    'FIL': 'Filosofia',
+                    'SIM': 'Simulado',
+                }.get(subject.upper(), subject.capitalize()),
+                'tps': tps,
+                'now': now(),
+                'questions': Question.objects.filter(tps=tps),
+            }
+            return render(request, 'tps_questions.html', data)
+        else:
+            if tps.start_date > now():
+                return render(request, 'feed.html', {'title': 'Opa!', 'description': 'Respostas serão liberadas apenas no dia {}.'.format(tps.start_date.strftime("%d/%m/%Y às %H:%M:%S"))})
+            if tps.end_date < now():
+                return render(request, 'feed.html', {'title': 'Opa!', 'description': 'Tempo limite de resposta excedido.'})
 
-        data = {
-            'id': id,
-            'subject_desc': {
-                'QUI': 'Química',
-                'BIO': 'Biologia',
-                'MAT': 'Matemática',
-                'FIS': 'Física',
-            }.get(subject.upper(), subject.capitalize()),
-            'tps': tps,
-            'questions': Question.objects.filter(tps=tps),
-        }
-        return render(request, 'tps_questions.html' if tps.questions else 'tps.html', data)
+            data = {
+                'id': id,
+                'subject_desc': {
+                    'QUI': 'Química',
+                    'BIO': 'Biologia',
+                    'MAT': 'Matemática',
+                    'FIS': 'Física',
+                    'FIL': 'Filosofia',
+                    'SIM': 'Simulado',
+                }.get(subject.upper(), subject.capitalize()),
+                'tps': tps,
+                'questions': Question.objects.filter(tps=tps),
+            }
+            return render(request, 'tps.html', data)
+            
     return render(request, 'feed.html', {'title': 'Opa!', 'description': 'Nenhum tps foi encontrado. Entre em contato com o responsável.'})
