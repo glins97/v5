57200ab893e89f0d9274bf8eda2a022f86c986d9
Gabriel Lins <gabriel.lins97@gmail.com>
2020-08-10 11:25:35 -0300

    FEAT: themes can now be sorted by jury

--- essay_manager/views/themes.py
+++ essay_manager/views/themes.py
@@ -8,6 +8,9 @@ from essay_manager.utils import get_user_details
 def themes_view(request):
     themes = []
     axes = {}
+    axes_enem = {}
+    axes_vunesp = {}
+    axes_cespe = {}
     for theme in Theme.objects.filter(active=True):
         if theme.axis not in axes:
             axes[theme.axis] = {
@@ -15,20 +18,66 @@ def themes_view(request):
                 'total_themes': 1, 
                 'done_themes': 0, 
             }
+            if theme.jury == 'ENEM':
+                axes_enem[theme.axis] = {
+                    'description': theme.axis, 
+                    'total_themes': 1, 
+                    'done_themes': 0, 
+                }
+            elif theme.jury == 'VUNESP':
+                axes_vunesp[theme.axis] = {
+                    'description': theme.axis, 
+                    'total_themes': 1, 
+                    'done_themes': 0, 
+                }
+            elif theme.jury == 'CESPE':
+                axes_cespe[theme.axis] = {
+                    'description': theme.axis, 
+                    'total_themes': 1, 
+                    'done_themes': 0, 
+                }
         else:
             axes[theme.axis]['total_themes'] += 1
+            if theme.jury == 'ENEM':
+               axes_enem[theme.axis]['total_themes'] += 1
+            elif theme.jury == 'VUNESP':
+               axes_vunesp[theme.axis]['total_themes'] += 1
+            elif theme.jury == 'CESPE':
+               axes_cespe[theme.axis]['total_themes'] += 1
+
         theme.completed = Essay.objects.filter(theme=theme, user=request.user).count() > 0
         if theme.completed:
             axes[theme.axis]['done_themes'] += 1
+            if theme.jury == 'ENEM':
+               axes_enem[theme.axis]['done_themes'] += 1
+            elif theme.jury == 'VUNESP':
+               axes_vunesp[theme.axis]['done_themes'] += 1
+            elif theme.jury == 'CESPE':
+               axes_cespe[theme.axis]['done_themes'] += 1
         themes.append(theme)
     
     axes_l = []
     for axis in sorted(axes.keys(), key=lambda axis: axis if axis != 'Outros eixos' else 'zzzzzzzzzzzzzzzzzzzz'):
         axes_l.append(axes[axis])
+        
+    axes_enem_l = []
+    for axis in sorted(axes_enem.keys(), key=lambda axis: axis if axis != 'Outros eixos' else 'zzzzzzzzzzzzzzzzzzzz'):
+        axes_enem_l.append(axes_enem[axis])
+        
+    axes_vunesp_l = []
+    for axis in sorted(axes_vunesp.keys(), key=lambda axis: axis if axis != 'Outros eixos' else 'zzzzzzzzzzzzzzzzzzzz'):
+        axes_vunesp_l.append(axes_vunesp[axis])
+        
+    axes_cespe_l = []
+    for axis in sorted(axes_cespe.keys(), key=lambda axis: axis if axis != 'Outros eixos' else 'zzzzzzzzzzzzzzzzzzzz'):
+        axes_cespe_l.append(axes_cespe[axis])
 
     data = {
         'title': 'Temas',
         'axes': axes_l,
+        'axes_enem': axes_enem_l,
+        'axes_vunesp': axes_vunesp_l,
+        'axes_cespe': axes_cespe_l,
         'themes': themes,
         'user': get_user_details(request.user),
     }
