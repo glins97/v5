4b65fb74ea138d316d74b1c4390e93e6d212d8c4
Gabriel Lins <gabriel.lins97@gmail.com>
2020-08-17 19:03:02 +0000

    FEAT: tps answers will now be replaced in case of re-submission

--- tps/apis.py
+++ tps/apis.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from tps.models import TPS, TPSAnswer, Question, QuestionAnswer
 from django.utils.timezone import now
 
+def get_or_create(class_, *args, **kwargs):
+    obj = class_.objects.filter(*args, **kwargs).first()
+    if not obj:
+        obj = class_(*args, **kwargs)
+        obj.save()
+    return obj
+
 def save_tps_answer(request, id):
     tpses = TPS.objects.filter(id=id)
     if tpses.count():
@@ -15,26 +22,19 @@ def save_tps_answer(request, id):
             return render(request, 'feed.html', {'title': 'Opa!', 'description': 'Tempo limite de resposta excedido.'})
         if not request.POST.get('name', False) or not request.POST.get('email', False):
             return render(request, 'feed.html', {'title': 'Opa!', 'description': 'Favor preencher nome e email!'})
-        if TPSAnswer.objects.filter(tps=tps, name=request.POST.get('name', '')).count():
-            if (now() - TPSAnswer.objects.filter(tps=tps, name=request.POST.get('name', '')).first().submission_date).seconds < 300:
-                return render(request, 'feed.html', {'title': 'Salvo!', 'description': 'O trabalho duro vence o talento.'})
-            return render(request, 'feed.html', {'title': 'Opa!', 'description': 'Resposta para "{}" cadastrada em {}.'.format(request.POST.get('name', ''), TPSAnswer.objects.filter(tps=tps, name=request.POST.get('name', '')).first().submission_date.strftime("%m/%d/%Y às %H:%M:%S"))})
-        if TPSAnswer.objects.filter(tps=tps, email=request.POST.get('email', '')).count():
-            if (now() - TPSAnswer.objects.filter(tps=tps, email=request.POST.get('email', '')).first().submission_date).seconds < 300:
-                return render(request, 'feed.html', {'title': 'Salvo!', 'description': 'O trabalho duro vence o talento.'})
-            return render(request, 'feed.html', {'title': 'Opa!', 'description': 'Resposta para "{}" cadastrada em {}.'.format(request.POST.get('email', ''), TPSAnswer.objects.filter(tps=tps, email=request.POST.get('email', '')).first().submission_date.strftime("%m/%d/%Y às %H:%M:%S"))})
-
-        tps_answer = TPSAnswer(
+      
+        tps_answer = get_or_create(TPSAnswer,
             tps=tps,
             name = request.POST.get('name', ''),
             email = request.POST.get('email', ''),
         )
+        tps_answer.grade = 0
         tps_answer.save()
         for attr in request.POST:
             if attr[0] == 'q':
                 number = int(attr[1:])
                 question = Question.objects.get(tps=tps, number=number)
-                QuestionAnswer(question=question, tps_answer=tps_answer, answer=request.POST[attr]).save()
+                get_or_create(QuestionAnswer, question=question, tps_answer=tps_answer, answer=request.POST[attr]).save()
                 if request.POST[attr][0] == question.correct_answer:
                     tps_answer.grade += 1
         
