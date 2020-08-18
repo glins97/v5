989a5811ae9f11ca9111d40d6e7f580020837e98
Gabriel Lins <gabriel.lins97@gmail.com>
2020-08-17 22:12:45 -0300

    IMPR: profile update now properly checks post variables before accessing it

--- essay_manager/apis/update_profile.py
+++ essay_manager/apis/update_profile.py
@@ -12,9 +12,16 @@ def update_profile_endpoint(request):
             setattr(profile, attr, request.POST[attr])
         profile.save()
 
-        request.user.first_name = request.POST['user__first_name']
-        request.user.last_name = request.POST['user__last_name']
-        request.user.save()
+        updated_user = False
+        if 'user__first_name' in request.POST:
+            updated_user = True
+            request.user.first_name = request.POST['user__first_name']
+        if 'user__last_name' in request.POST:
+            updated_user = True
+            request.user.last_name = request.POST['user__last_name']
+        
+        if updated_user:
+            request.user.save()
         return redirect('/profile/?updated=True')
     except Exception as e:
         logger.error(f'Error updating profile. Error {e}', exc_info=e)
